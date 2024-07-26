3a51b83892e55b5af931944d1090999c19e3ab62
Armalor <armalor@gmail.com>
2024-07-26 13:09:40 +0300

    Два знака

--- pic.py
+++ pic.py
@@ -3,17 +3,36 @@ import cv2
 import os
 
 img = cv2.imread('./data/road-signs/footpath.jpg')
-img2 = cv2.imread('./data/road-signs/left-turn.jpg')
+# img2 = cv2.imread('./data/road-signs/left-turn.jpg')
 
 if img is None:
     print('ФАЙЛ НЕ НАЙДЕН')
     os._exit(1)
 
 
+
+
+
+
+# cv2.imshow('img2', img2)
+
+print(img.shape)
+print(img.size)
+# uint8:
+print(img.dtype)
+
+x_max, y_max, _ = img.shape
+
+x = x_max // 3
+y = y_max // 3
+
+img[x, y] = [0, 0, 255]
+tpl = (img.item(x, y, 0), img.item(x, y, 1), img.item(x, y, 2))
+
+print(tpl)
+
+
 cv2.imshow('img', img)
-cv2.imshow('img2', img2)
-
-
 while True:
     key = cv2.waitKey(1)
     if key == 27:
