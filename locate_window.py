2cce4c227fb02fa9c5b0b37f89c19e8710e9b818
Armalor <armalor@gmail.com>
2024-07-27 13:19:46 +0300

    Ищем окошко NFS

---
+++ locate_window.py
@@ -0,0 +1,55 @@
+import pyautogui
+import time
+import numpy as np
+import cv2
+import imutils
+
+# Ждем три секунды, успеваем переключиться на окно:
+print('waiting for 2 seconds...')
+time.sleep(2)
+
+#ВНИМАНИЕ! PyAutoGUI НЕ ЧИТАЕТ В JPG!
+title = './nfs-shift-title.png'
+
+nfs_window_location = None
+searching_attempt = 1
+while searching_attempt <= 5:
+    nfs_window_location = pyautogui.locateOnScreen(title)
+
+    if nfs_window_location is not None:
+        print('nfs_window_location = ', nfs_window_location)
+        break
+    else:
+        searching_attempt += 1
+        time.sleep(1)
+        print("attempt %d..." % searching_attempt)
+
+if nfs_window_location is None:
+    print('NFS Window not found')
+    exit(1)
+
+# Извлекаем из картинки-скриншота только данные окна NFS.
+# Наша target-картинка - это заголовочная полоска окна.
+# Для получения скриншота, мы берем ее левую точку (0),
+# а к верхней (1) прибавляем высоту (3)
+left = nfs_window_location[0]
+top = nfs_window_location[1]+nfs_window_location[3]
+
+# ВНИМАНИЕ!  У вас, скорее всего, будет другое разрешение, т.к. у меня 4К-монитор!
+# Здесь надо выставить те параметры, которые вы задали в игре.
+window_resolution = (1920, 1080)
+
+window = (left, top, left+window_resolution[0], top+window_resolution[1])
+
+cv2.namedWindow('result')
+
+while True:
+
+    pix = pyautogui.screenshot(region=(left, top, window_resolution[0], window_resolution[1]))
+    numpix = cv2.cvtColor(np.array(pix), cv2.COLOR_RGB2BGR)
+
+    cv2.imshow('result', numpix)
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.destroyAllWindows()
