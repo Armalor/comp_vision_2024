d907ca03c3aaa06d6ec0349e5b6ef567d3b9823d
Armalor <armalor@gmail.com>
2024-07-27 11:02:18 +0300

    Выделение цвета

--- video.py
+++ video.py
@@ -11,7 +11,19 @@ while True:
 
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('result', frame_hsv)
+    frame_h = frame_hsv[:, :, 0]
+    frame_s = frame_hsv[:, :, 1]
+    frame_v = frame_hsv[:, :, 2]
+
+    min_ = (40, 0, 0)
+    max_ = (75, 255, 255)
+
+    mask = cv2.inRange(frame_hsv, min_, max_)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('mask', mask)
+    cv2.imshow('result', result)
 
     if cv2.waitKey(1) == 27:
         break
