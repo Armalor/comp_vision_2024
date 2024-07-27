e271acf002282afc68720c4d69bc8bff8dd0acb5
Armalor <armalor@gmail.com>
2024-07-27 12:23:52 +0300

    Ищем контуры

--- video.py
+++ video.py
@@ -5,8 +5,8 @@ cap = cv2.VideoCapture(0)
 cv2.namedWindow('result')
 
 ranges = {
-    'min_h1': {'current': 20, 'max': 180},
-    'max_h1': {'current': 40, 'max': 180},
+    'min_h1': {'current': 70, 'max': 180},
+    'max_h1': {'current': 85, 'max': 180},
 }
 
 
@@ -38,11 +38,40 @@ while True:
     max_ = (ranges['max_h1']['current'], 255, 255)
 
     mask = cv2.inRange(frame_hsv, min_, max_)
-
     result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # Ищем контуры
+    contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # Сами структуры контуров хранятся в начальном элементе возвращаемого значения:
+    contours = contours[0]
+
+    # Их, кстати, может и не быть:
+    if contours:
+
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+        # Третий аргумент — это индекс контура, который мы хотим вывести. Мы хотим самый большой.
+        # Вывести все можно, передав -1 вместо 0:
+        cv2.drawContours(result, contours, 0, (255, 0, 0), 1)
+
+        # Получаем прямоугольник, обрамляющий наш контур:
+        (x, y, w, h) = cv2.boundingRect(contours[0])
+
+        # И выводим его:
+        cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+        # Аналогично строим минимальную описанную вокруг наибольшего контура окружность:
+        (x1, y1), radius = cv2.minEnclosingCircle(contours[0])
+        center = (int(x1), int(y1))
+        radius = int(radius)
+        cv2.circle(result, center, radius, (0, 255, 0), 1)
+
 
     cv2.imshow('mask', mask)
     cv2.imshow('result', result)
+
+
+
 
     if cv2.waitKey(1) == 27:
         break
